41d89c4124013f323bcefc978cc084ff62172ab2
toxicpuppy123@gmail.com <toxicpuppy123@gmail.com>
2022-10-10 13:48:40 -0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Cows say \"moo\"!");
-        System.out.println("Brooklyn Tech\n29 Ft. Greene\nBrooklyn");
-        System.out.println("An \"i\"\nfor\nan eye");
-        System.out.println("The string \"\"\nis an empty string.");
-        System.out.println("The escape sequence for \\ is \\\\");
-        System.out.println("The escape sequence for \" is \\\"");
-        System.out.println("/A\\V/A\\\n\\V/A\\V/");
-        System.out.println("Use \"\\" + "n" + "\"\nfor a new line");
-        System.out.println("My favorite punctuation mark is \"\\\"");
-        System.out.println("Now I know my \nA\n B\n  C’ s");
-        System.out.println("¯\\_(\"/_/¯"    );
-        System.out.println("Dear Jen,\nYou got an \"A\"!\n\nSincerely,\nThe teacher ");
-        System.out.println(" * *\n* * *\n * *");
-        System.out.println("I hate \"escape characters\"\n:\\");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        BabyParrot baby = new BabyParrot("Toucan", 3, 4.25);
+        baby.setName("Lil' Beans");
+        String info = baby.parrotInfo(); // store returned string in variable
+        System.out.println(info);        // print the returned string
 
 
 
